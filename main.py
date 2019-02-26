273ecbd10df1cca4490d4282b745e586a5aebe97
Toan Phung <toan.phung@uqconnect.edu.au>
2019-02-26 23:24:58 +1000

    Update main.py
    adding wide to long form transformation

--- main.py
+++ main.py
@@ -18,9 +18,15 @@ print(measles_1["Measles-containing-vaccine first-dose (MCV1) immunization cover
 
 # Index type is immutable so we can't just change the value within the index
 # Index have to be unique
-new_index = ["M1"]
 
-measles_1.columns = measles_1.columns.set_levels(np.array(new_index), level=0)
+measles_1.columns = measles_1.columns.set_levels(["M1"], level=0)
+
+measles_1 = measles_1.stack()
+print(measles_1)
+
+measles_1.index.names = ["Country", "Year"]
+
+long_measles_1 = measles_1.reset_index()
 
 measles_2 = pd.read_csv("./data/MCV2.csv", header=[0, 1], index_col=0)
 
