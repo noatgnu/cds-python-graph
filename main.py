a1f425fc10c35baa9c7810352ab85cddc445c004
Toan Phung <toan.phung@uqconnect.edu.au>
2019-03-02 08:20:57 +1000

    update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print(measles_1["Measles-containing-vaccine first-dose (MCV1) immunization cover
 # Index have to be unique
 
 measles_1.columns = measles_1.columns.set_levels(["M1"], level=0)
-
+measles_1.columns = measles_1.columns.set_levels([int(i.strip()) for i in measles_1["M1"].columns], level=1)
 measles_1 = measles_1.stack()
 print(measles_1)
 
@@ -31,3 +31,14 @@ long_measles_1 = measles_1.reset_index()
 measles_2 = pd.read_csv("./data/MCV2.csv", header=[0, 1], index_col=0)
 
 measles_combined = measles_1.join(measles_2)
+
+def replace(x):
+    if type(x) is str:
+        if " " in x:
+            return x.split(" ")[0]
+        else:
+            return x
+    else:
+        return x
+
+measles_1.applym(lambda x: x if type(x) is not str else (x.split()[0] if " " in x else x))
